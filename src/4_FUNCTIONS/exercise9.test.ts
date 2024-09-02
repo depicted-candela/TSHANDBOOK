9de259bd644b25142b97b37aced76e001a6ed2b1
depiction <depiction@debian.com>
2024-09-01 20:19:47 -0400

    9th exercise from chapter 4 finished

---
+++ src/4_FUNCTIONS/exercise9.test.ts
@@ -0,0 +1,16 @@
+import translate, {Differential, Shape} from "./exercise9";
+
+describe('Testing union of types for a function that moves a shape differentially or scaled', () => {
+    test('Test for differential movement', () => {
+        const shape: Shape = {x: 1, y: 1};
+        const differential: Differential = {dx: 0.1, dy: 0.1};
+        translate(shape, differential);
+        expect(shape).toEqual({x: 1.1, y: 1.1});
+    });
+    test('Test for scaled or fixed movement', () => {
+        const shape: Shape = {x: 1, y: 1};
+        const fixedOrScaledMovement: number = 0.1;
+        translate(shape, fixedOrScaledMovement);
+        expect(shape).toEqual({x: 1.1, y: 1.1});
+    });
+});
